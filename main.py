7d68ee95e294a3cc1a2303dfd462a35c8b82a24f
Kazi Sahib <kazisahib5@gmail.com>
2024-05-01 01:15:09 +0600

    -1.0

---
+++ main.py
@@ -0,0 +1,36 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+# pylint: disable=import-error
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+
+data = pd.read_csv("car_purchasing.csv")
+data.head()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data[['age', 'annual Salary', 'credit card debt', 'net worth']], data.amount, test_size=0.2, random_state=42)
+print(X_train)
+input_layer = Input(shape=(4,))
+hidden_layer = Dense(64, activation='relu')(input_layer)
+output_layer = Dense(1, activation='linear')(hidden_layer)
+
+model = Model(inputs=input_layer, outputs=output_layer)
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
+model.fit(X_train, y_train, epochs=5000, batch_size=30, validation_split=0.2)
+loss = model.evaluate(X_test, y_test, batch_size=30, verbose=1)
+print(loss)
+
+y_pred = model.predict(X_test)
+print(pd.DataFrame({'Actual': y_test, 'Predicted': y_pred.flatten()}).head())
+
+plt.scatter(y_test, y_pred, color='red')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], color='blue', linewidth=2)
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('Actual vs Predicted')
+plt.show()
+
